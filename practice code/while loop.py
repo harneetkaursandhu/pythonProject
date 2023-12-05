cc28455d374d17a68ef79c9a9adcd98e4d015850
Harneet <harneet.sandhu@developersinstitute.co.nz>
2023-12-05 21:06:13 +1300

    Add while loop file

---
+++ practice code/while loop.py
@@ -0,0 +1,10 @@
+# Write a program with a while loop that prints 1 through to n in square brackets. For example, if the user enters 6
+# then the program should display
+# [1] [2] [3] [4] [5] [6]
+
+is_running = True
+number = int(input("Please enter a number\n\n"))
+counter = 0
+while counter < number:
+    print("[", counter + 1, "] ", end="")
+    counter += 1
