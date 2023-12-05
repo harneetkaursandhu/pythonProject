f1153b1183cd5e9543e537cdfb3cd6e2b36bbd44
Harneet <harneet.sandhu@developersinstitute.co.nz>
2023-12-05 21:32:32 +1300

    Add for loop1 file

---
+++ practice code/for loop1.py
@@ -0,0 +1,8 @@
+# "Write a program that will ask the user for a message and the number of times they want
+# that message displayed. Then output the message that number of times."
+
+user_input = int(input("Please enter the number of times "
+                       "that you wish to see the message."))
+
+for i in range(user_input):
+    print("hello world....", i)
